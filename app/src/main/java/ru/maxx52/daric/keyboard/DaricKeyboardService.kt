439fadf6a22850ad03d2d864cd1a70d3efe5083f
maxx52 <gospelwork@yandex.ru>
2026-08-23 17:33:01 +0700

    Define Russian and English keyboard layouts

--- app/src/main/java/ru/maxx52/daric/keyboard/DaricKeyboardService.kt
+++ app/src/main/java/ru/maxx52/daric/keyboard/DaricKeyboardService.kt
@@ -459,17 +459,30 @@ class DaricKeyboardService : InputMethodService() {
 
         val numberRow = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 
-        val letterRows = listOf(
+        val russianLetterRows = listOf(
             listOf("й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"),
             listOf("ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э"),
             listOf("⇧", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "⌫"),
             listOf("?123", "🌐", ",", "пробел", ".", "↵")
         )
 
-        val symbolRows = listOf(
+        val englishLetterRows = listOf(
+            listOf("q", "w", "e", "r", "t", "y", "u", "i", "o", "p"),
+            listOf("a", "s", "d", "f", "g", "h", "j", "k", "l"),
+            listOf("⇧", "z", "x", "c", "v", "b", "n", "m", "⌫"),
+            listOf("?123", "🌐", ",", "space", ".", "↵")
+        )
+
+        val russianSymbolRows = listOf(
             listOf("@", "#", "₽", "_", "&", "-", "+", "(", ")", "/"),
             listOf("*", "\"", "'", ":", ";", "!", "?", "⌫"),
             listOf("АБВ", "🌐", ",", "пробел", ".", "↵")
+        )
+
+        val englishSymbolRows = listOf(
+            listOf("@", "#", "$", "_", "&", "-", "+", "(", ")", "/"),
+            listOf("*", "\"", "'", ":", ";", "!", "?", "⌫"),
+            listOf("ABC", "🌐", ",", "space", ".", "↵")
         )
     }
 }
